ad63fdb110f3a0e217836fd9c4c7949443c4b23e
Maxim-Maltsov <60138910+Maxim-Maltsov@users.noreply.github.com>
2023-07-23 13:18:23 +0700

    Внесены правки в метод "finishRoundIfNeeded()" в классе "Game".
    Внесены правки в метод "finishRoundIfNeeded()" в классе "Game". Теперь активную игру для изменения атрибутов игры получаем до изменения атрибутов активного раунда.

--- app/Models/Game.php
+++ app/Models/Game.php
@@ -311,15 +311,15 @@ class Game extends Model
             $draw = ($winner_id == Game::NO_WINNER)? Game::YES : Game::NO;
             
             $activeRound = $this->getActiveRound();
-
             $game = $activeRound->game;
-            $game->need_start_new_round = Game::YES;
-            $game->save();
 
             $activeRound->winned_player = $winnedPlayer;
             $activeRound->draw = $draw;
             $activeRound->status = Round::FINISHED;
             $activeRound->save();
+
+            $game->need_start_new_round = Game::YES;
+            $game->save();
 
             GameRoundFinishedEvent::dispatch(GameResource::make($this));
         }
