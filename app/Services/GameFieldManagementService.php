fe3a8424654867815b10c849b38e4abad255a80d
Maxim Maltsov <maltsovm@gmail.com>
2023-08-01 18:26:01 +0700

    Добавлен метод "needShowPlayerWaitingBlock()"

--- app/Services/GameFieldManagementService.php
+++ app/Services/GameFieldManagementService.php
@@ -2,10 +2,32 @@
 
 namespace App\Services;
 
+use App\Models\Game;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
 
+/**
+ * Controls the display of user interface elements.
+ */
 class GameFieldManagementService extends Model
 {
     use HasFactory;
+
+    /**
+     * Determines whether to show a block with information about waiting for the second player.
+     */
+    public static function needShowPlayerWaitingBlock(): bool
+    {   
+        // Перенести запрос в класс "gameRepository". Получаем игру в которой игровой статус 1-го игрока равен 'WAITING_PLAYER'. 1-ый игрок ожидает 2-го игрока. 
+        $game = Game::where('status', Game::WAITING_PLAYER)
+                    ->where('player_1', Auth::id())
+                    ->first();
+        
+        if ($game->isEmpty()) {
+            return false;
+        }
+
+        return true;
+    }
 }
