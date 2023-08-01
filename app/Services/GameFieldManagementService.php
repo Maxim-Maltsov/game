41d1c25f3d9172d96d75fb3052f0e3fc1516b4dd
Maxim Maltsov <maltsovm@gmail.com>
2023-08-01 18:38:48 +0700

    Добавлен метод "needShowBlockWithOfferToPlay()"

--- app/Services/GameFieldManagementService.php
+++ app/Services/GameFieldManagementService.php
@@ -30,4 +30,22 @@ class GameFieldManagementService extends Model
 
         return true;
     }
+
+    /**
+     * Determines whether to show a block with an offer to play the game to the second player.
+     */
+    public static function needShowBlockWithOfferToPlay(): bool
+    {   
+        // Перенести запрос в класс "gameRepository". Получаем игру в которой игровой статус 2-го игрока равен 'WAITING_PLAYER'. 2-й игрок ещё не ответил на предложения 1-го принять участие в игре.
+        $game = Game::where('status', Game::WAITING_PLAYER)
+                    ->where('player_2', Auth::id())
+                    ->first();
+       
+        if ($game->isEmpty()) {       
+            return false;
+        }
+
+        return true;
+    }
+
 }
