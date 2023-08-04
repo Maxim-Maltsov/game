0eb2155b3134078c0da8c735561b0d556dc6e391
Maxim Maltsov <maltsovm@gmail.com>
2023-08-04 20:13:12 +0700

    Создан метод "updateUserList()".

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -2,13 +2,16 @@
 
 namespace App\Services;
 
+use App\Events\AmountUsersOnlineChangedEvent;
+use App\Http\Resources\UserCollection;
 use App\Models\User;
+use App\Repositories\UserRepository;
 
 /**
  * Contains methods for updating user data and other business logic related to users.
  */
 class UserService 
-{
+{   
     /**
      * Update the user's game status to free.
      */
@@ -16,5 +19,18 @@ class UserService
     {
         $Player->game_status = User::FREE;
         $Player->save();
+    }
+
+    /**
+     * Update the list of users who are online.
+     */
+    public function updateUserList(): void
+    {   
+        $userRepository = new UserRepository();
+        $users = $userRepository->getEveryoneWhoOnlineWithPaginated(4);
+
+        if ($users->isNotEmpty()) {
+            AmountUsersOnlineChangedEvent::dispatch(UserCollection::make($users));
+        }
     }
 }
