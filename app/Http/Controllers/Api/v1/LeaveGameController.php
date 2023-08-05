5cf01991b71d5353826a614e179b00085ef86487
Maxim Maltsov <maltsovm@gmail.com>
2023-08-05 15:00:52 +0700

    Список пользователей обновляет "updateUserList()".

--- app/Http/Controllers/Api/v1/LeaveGameController.php
+++ app/Http/Controllers/Api/v1/LeaveGameController.php
@@ -3,26 +3,20 @@
 namespace App\Http\Controllers\Api\v1;
 
 use App\Actions\LeaveGameAction;
-use App\Events\AmountUsersOnlineChangedEvent;
 use App\Http\Resources\GameResource;
-use App\Http\Resources\UserCollection;
 use App\Models\Game;
-use App\Repositories\UserRepository;
+use App\Services\UserService;
 
 /**
  * Handles a request to leave the game before it ends.
  */
 class LeaveGameController 
 {
-    public function __invoke(Game $game, LeaveGameAction $leaveGame, UserRepository $userRepository) :GameResource
+    public function __invoke(Game $game, LeaveGameAction $leaveGame, UserService $userService): GameResource
     {
         $leaveGame->handle($game);
-
-        // Getting a list of "online" users and passing it through the "AmountUsersOnlineChangedEven" event to the client side for further rendering.
-        $users = $userRepository->getEveryoneWhoOnlineWithPaginated(4); 
-        AmountUsersOnlineChangedEvent::dispatch(UserCollection::make($users));
-
+        $userService->updateUserList();
+   
         return GameResource::make($game);
-        
     }
 }
