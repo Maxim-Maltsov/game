16b84ef6b23941b3b1c7e37d2e2cfd62a2703a50
Maxim Maltsov <maltsovm@gmail.com>
2023-08-05 18:09:48 +0700

    Создан класс "RoundRepository".

---
+++ app/Repositories/RoundRepository.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Round as Model;
+use App\Models\Round;
+
+/**
+ * Repository for working with the round. Can output datasets. Cannot create/modify rounds.
+ */
+class RoundRepository extends CoreRepository
+{
+    /**
+     * Returns a string with the model class.
+     */
+    public function getModelClass(): string
+    {
+        return Model::class;
+    }
+
+    /**
+     * Get an active round of the game.
+     */
+    public function getActiveRound(int $gameId): Round
+    {   
+        $columns = ['id', 'game_id', 'number', 'status', 'winned_player', 'draw'];
+       
+        $result = $this->startConditions()
+                       ->where('game_id', $gameId)
+                       ->where('status', Model::NO_FINISHED)
+                       ->select($columns)
+                       ->first();
+       
+        return $result;
+    }
+
+}
