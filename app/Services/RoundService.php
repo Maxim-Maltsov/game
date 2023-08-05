a1092588fb50ddcab85f7546f683b8109ab25ce8
Maxim Maltsov <maltsovm@gmail.com>
2023-08-05 19:56:11 +0700

    Создан метод "finishRoundEarly"

--- app/Services/RoundService.php
+++ app/Services/RoundService.php
@@ -11,6 +11,14 @@ use App\Repositories\RoundRepository;
  */
 class RoundService 
 {   
-
+    /**
+     * Finishes the round in case of early exit of one of the players from the game.
+     */
+    public function finishRoundEarly(Round $activeRound, array $players): void
+    {
+        $activeRound->status = Round::FINISHED;
+        $activeRound->winned_player = $players['winned_player']; 
+        $activeRound->save();
+    }
 
 }
