ddb021a186e55c98669ed272a2c3bc5645a35091
Maxim Maltsov <maltsovm@gmail.com>
2023-08-05 15:18:00 +0700

    Создан класс "RefereeService"

---
+++ app/Services/RefereeService.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Services;
+
+/**
+ * Defines the winner of the game, the winner of the round. Monitors compliance with all the rules of the game.
+ */
+class RefereeService 
+{
+
+}
