68f6d27c8ece65f1c4595583e6c87912f29f8d91
Maxim Maltsov <maltsovm@gmail.com>
2023-08-05 19:13:16 +0700

    Создан класс "RoundService".

---
+++ app/Services/RoundService.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Services;
+
+
+use App\Models\Round;
+use App\Repositories\RoundRepository;
+
+/**
+ * Contains methods for updating round data and other business logic related to rounds.
+ */
+class RoundService 
+{   
+
+
+}
