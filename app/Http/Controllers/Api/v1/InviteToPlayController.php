1311a1efdd6948fe1eb0bdacab3aa9d846a30e7b
Maxim Maltsov <maltsovm@gmail.com>
2023-08-06 12:53:16 +0700

    Создан "InviteToPlayController".

---
+++ app/Http/Controllers/Api/v1/InviteToPlayController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1;
+
+
+
+/**
+ * Handles a request to invite a second player to take part in the game.
+ */
+class InviteToPlayController 
+{
+    public function __invoke()
+    {
+     
+    }
+}
