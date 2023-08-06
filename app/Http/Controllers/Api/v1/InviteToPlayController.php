5422a8257aab435d5ea9e488a48efcc09c31f578
Maxim Maltsov <maltsovm@gmail.com>
2023-08-06 22:10:05 +0700

    Создан класс "InviteToPlayController".

--- app/Http/Controllers/Api/v1/InviteToPlayController.php
+++ app/Http/Controllers/Api/v1/InviteToPlayController.php
@@ -2,15 +2,34 @@
 
 namespace App\Http\Controllers\Api\v1;
 
-
+use App\Actions\InviteToPlayAction;
+use App\Exceptions\PlayerNotFoundException;
+use App\Http\Requests\GameRequest;
+use App\Services\UserService;
+use Exception;
+use Illuminate\Support\Facades\Log;
 
 /**
  * Handles a request to invite a second player to take part in the game.
  */
 class InviteToPlayController 
 {
-    public function __invoke()
-    {
-     
+    public function __invoke(GameRequest $request, InviteToPlayAction $inviteToPlay, UserService $userService)
+    {   
+        try {
+
+            $game = $inviteToPlay->handle($request->player_2);
+            $userService->updateUserList();
+           
+            return $game;
+        }
+        catch (Exception $e) {
+
+            return response()->json([ 'data' => [
+                
+                'message' => $e->getMessage(),
+                'exception' => true,
+            ]]);
+        }
     }
 }
