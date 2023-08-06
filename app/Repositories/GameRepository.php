085996619699ef3ca1bbf5f9b87e610b9ba094ea
Maxim Maltsov <maltsovm@gmail.com>
2023-08-06 18:14:46 +0700

    Добавлены методы получения игры

--- app/Repositories/GameRepository.php
+++ app/Repositories/GameRepository.php
@@ -16,4 +16,54 @@ class GameRepository extends CoreRepository
     {
         return Model::class;
     }
+
+    /**
+    * Gets the game the user is in.
+    */
+    public function getGameWhereUserIsIn(int $id): ?Model
+    {   
+        $columns = ['id', 'player_1', 'player_2', 'status', 'winned_player', 'leaving_player'];
+       
+        $result = $this->startConditions()
+                       ->where('player_1', $id)
+                       ->orWhere('player_2', $id)
+                       ->select($columns)
+                       ->latest()
+                       ->first();
+
+        return $result;
+    }
+   
+    /**
+     * Gets the game where the user is the first player.
+     */
+    public function getGameWhereUserIsFirstPlayer(int $id): ?Model
+    {   
+        $columns = ['id', 'player_1', 'player_2', 'status', 'winned_player', 'leaving_player'];
+       
+        $result = $this->startConditions()
+                       ->whereIn('status', [Model::WAITING_PLAYER, Model::IN_PROCESS])
+                       ->where('player_1', $id)
+                       ->select($columns)
+                       ->first();
+
+        return $result;
+    }
+
+    /**
+     * Gets the game where the user is the second player.
+     */
+    public function getGameWhereUserIsSecondPlayer(int $id): ?Model
+    {   
+        $columns = ['id', 'player_1', 'player_2', 'status', 'winned_player', 'leaving_player'];
+       
+        $result = $this->startConditions()
+                       ->whereIn('status', [Model::WAITING_PLAYER, Model::IN_PROCESS])
+                       ->where('player_2', $id)
+                       ->select($columns)
+                       ->first();
+
+        return $result;
+    }
+    
 }
