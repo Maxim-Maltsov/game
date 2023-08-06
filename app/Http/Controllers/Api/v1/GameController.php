b8a69c49dab217bf1b8b5b7e25c994e449f09bba
Maxim Maltsov <maltsovm@gmail.com>
2023-08-06 12:11:50 +0700

    Удалён метод "leaveGame()".

--- app/Http/Controllers/Api/v1/GameController.php
+++ app/Http/Controllers/Api/v1/GameController.php
@@ -79,18 +79,6 @@ class GameController extends Controller
        return User::play($game);
     }
     
-
-    // public function leaveGame(Game $game)
-    // {  
-    //     $game = User::leave($game);
-        
-    //     // Getting a list of "online" users and passing it through the "AmountUsersOnlineChangedEven" event to the client side for further rendering.
-    //     $users = $this->userRepository->getEveryoneWhoOnlineWithPaginated(4); 
-    //     AmountUsersOnlineChangedEvent::dispatch(UserCollection::make($users));
-
-    //     return $game;
-    // }
-
     
     public static function makeMove(MoveRequest $request) 
     {
