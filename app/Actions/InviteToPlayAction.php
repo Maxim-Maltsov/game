cc580ff59a8dcb33802131824cc063b5029a68eb
Maxim Maltsov <maltsovm@gmail.com>
2023-08-06 12:54:12 +0700

    Создан класс "InviteToPlayAction".

---
+++ app/Actions/InviteToPlayAction.php
@@ -0,0 +1,19 @@
+<?php 
+
+namespace App\Actions;
+
+
+
+/**
+ * Sends an invitation to the second player to take part in the game.
+ */
+class InviteToPlayAction
+{   
+    /**
+    * Triggers the action to invitation to the game.
+    */
+    public function handle(): void
+    {   
+        
+    }
+}
